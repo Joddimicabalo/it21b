34d2aaa98c9374b988c817c4b02720fd0ad71409
Joddimicabalo <joddivicentemicabalo@gmail.com>
2025-09-06 19:34:45 +0800

    dev-arithmetic

--- dev-arithmetic.js
+++ dev-arithmetic.js
@@ -7,3 +7,13 @@ sum = num1 = num2;
 diff = num1-num2;
 prod = num1 = num2;
 quo = num1 / num2;
+
+console.log("hello world")
+
+num1 = 2
+num2 = 3
+
+sum = num1 = num2;
+diff = num1-num2;
+prod = num1 = num2;
+quo = num1 / num2;
